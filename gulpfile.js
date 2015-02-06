9d412d4c26a0461c8299a6f877ed5324735b7941
paul christophe <paul.christophe6@gmail.com>
2015-02-06 15:19:40 -0500

    script tag

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,26 @@ var gulp = require('gulp'),
 
 
 gulp.task('browserify', ['clean'], function () {
-  browserify('./src/js/app.js')
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
+	browserify('./src/js/app.js')
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('connect', function() {
-  connect.server();
+	connect.server();
 });
 
 gulp.task('clean', function() {
-    gulp.src('./dist/*')
-      .pipe(clean({force: true}));
+	gulp.src('./dist/*')
+		.pipe(clean({force: true}));
 });
 
 // Rerun tasks whenever a file changes.
 gulp.task('watch', function() {
-  gulp.watch('./src/js/*.js', ['browserify']);
+	gulp.watch('./src/js/*.js', ['browserify']);
 });
  
 gulp.task('default', ['watch', 'browserify', 'connect']);
